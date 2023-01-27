69c290ce6165a703289097270573adbb5d6c2207
arno-solo <arno756@outlook.com>
2023-01-27 13:46:16 +0800

    init

--- AlamofireWithAsyncAwait/ContentView.swift
+++ AlamofireWithAsyncAwait/ContentView.swift
@@ -7,15 +7,55 @@
 
 import SwiftUI
 
+class ContentViewViewModel: ObservableObject {
+    @MainActor @Published var errorMessage = ""
+    @MainActor @Published var appliances: [Appliance] = []
+
+    func fetchAppliances() async {
+        await MainActor.run {
+            self.errorMessage = ""
+        }
+        if let res = await NetworkAPI.getAppliances() {
+            await MainActor.run {
+                self.appliances = res
+            }
+        } else {
+            await MainActor.run {
+                self.errorMessage = "Fetch data failed"
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = ContentViewViewModel()
+
     var body: some View {
-        VStack {
+        VStack(spacing: 16) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if viewModel.errorMessage != "" {
+                Text(viewModel.errorMessage)
+            }
+            Button("Fetch") {
+                Task {
+                    await viewModel.fetchAppliances()
+                }
+            }
+            List {
+                ForEach(viewModel.appliances, id: \.id) { item in
+                    Text("\(item.brand) - \(item.equipment)")
+                }
+            }
+            .listStyle(.inset)
         }
         .padding()
+        .onAppear {
+            Task {
+                await viewModel.fetchAppliances()
+            }
+        }
     }
 }
 
---
+++ AlamofireWithAsyncAwait/Models/Appliance.swift
@@ -0,0 +1,15 @@
+//
+//  Appliance.swift
+//  AlamofireWithAsyncAwait
+//
+//  Created by Arno Solo on 1/27/23.
+//
+
+import Foundation
+
+struct Appliance: Identifiable, Codable {
+    let id: Int
+    let uid: String
+    let brand: String
+    let equipment: String
+}

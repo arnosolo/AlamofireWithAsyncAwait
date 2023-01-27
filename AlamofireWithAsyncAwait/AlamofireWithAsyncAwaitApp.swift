5aeb4d7b7460aaaa9e44d401d70d8f36119f0ccd
arno-solo <arno756@outlook.com>
2023-01-27 12:53:06 +0800

    Initial Commit

---
+++ AlamofireWithAsyncAwait/AlamofireWithAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireWithAsyncAwaitApp.swift
+//  AlamofireWithAsyncAwait
+//
+//  Created by Arno Solo on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireWithAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
